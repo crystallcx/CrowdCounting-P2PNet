f1cf3501edc55c401893aef61bf0d7971d360e03
Crystal Lee <n10203478@cl4n005.ib0.hpc.qut.edu.au>
2022-08-09 23:18:09 +1000

    add additional dataset that accepts empty annotations

--- crowd_datasets/__init__.py
+++ crowd_datasets/__init__.py
@@ -3,5 +3,13 @@ def build_dataset(args):
     if args.dataset_file == 'SHHA':
         from crowd_datasets.SHHA.loading_data import loading_data
         return loading_data
+        
+    elif args.dataset_file == 'Wildlife':
+        from crowd_datasets.Wildlife.loading_data import loading_data
+        return loading_data
+
+    elif args.dataset_file == 'SHAA_edited':
+        from crowd_datasets.SHAA_edited.loading_data import loading_data
+        return loading_data
 
     return None
