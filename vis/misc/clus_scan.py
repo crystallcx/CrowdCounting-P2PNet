cacf5f857d33511e81fd23373611d198a1cc10f2
crystallcx <crystaltienlee@gmail.com>
2022-08-22 16:40:20 +1000

    update gitignore & gridscan script

--- vis/misc/clus_scan.py
+++ vis/misc/clus_scan.py
@@ -15,6 +15,7 @@ from sklearn.preprocessing import StandardScaler
 
 db_dir = "/home/n10203478/EGH400/database/"
 p2p_dir = "/home/n10203478/EGH400/koaladetection/CrowdCounting-P2PNet" 
+p2ptr_names=['run_1','run_2', 'run_3', 'run_4', 'run_5take2']
 
 with open(os.path.join(db_dir,"test_gt.txt")) as load_file:
     gt_test = [tuple(line.split()) for line in load_file]
@@ -30,14 +31,13 @@ for file in glob.glob(os.path.join(db_dir,"test/*.png")):
             count = line[1]
             test_array.append(tuple([pre,count,img]))   
 
-len(test_array)
-def get_args_parser():
-    parser = argparse.ArgumentParser('Set params', add_help=False)
-    parser.add_argument('--output_dir', default='../../database2/',
-                        help='path where to save dataset')
-    parser.add_argument('--source_dir', default='../../images/',
-                        help='path where image database is located')
-    return parser
+# def get_args_parser():
+#     parser = argparse.ArgumentParser('Set params', add_help=False)
+#     parser.add_argument('--p2p_dir', default="/home/n10203478/EGH400/koaladetection/CrowdCounting-P2PNet" ,
+#                         help='path where p2p repo is located')
+#     parser.add_argument('--database_dir', default="/home/n10203478/EGH400/database/",
+#                         help='path where image database is located')
+#     return parser
 
 def column(matrix, i):
     return [row[i] for row in matrix]
@@ -50,12 +50,12 @@ def cluster_img(k, eps=5, min_samples = 3): #outputs filename, no.clusters, mean
     filename = test_array[k][0]+".png"
 
     # Obtain pred points from all 5 trains and store in array X
-    for i in range(5): 
-        pts_path = (os.path.join(p2p_dir,"vis","run_"+str(i+1),test_array[k][0]+'.txt'))
+    for i in range(len(p2ptr_names)): 
+        pts_path = (os.path.join(p2p_dir,"vis",p2ptr_names[i],test_array[k][0]+'.txt'))
         with open(pts_path) as load_file:
             pts.append([(line.split()) for line in load_file])
 
-    for i in range(5): 
+    for i in range(len(p2ptr_names)): 
         for p in pts[i]:
             x = int(float(p[0])*640)
             y = int(float(p[1])*512)
@@ -85,49 +85,51 @@ def cluster_all(eps, min_samples,length):
         cluster_points.append(cluster_img(m, eps, min_samples))
     return cluster_points
 
-def main(args):
-    gt_pts_test = []
-    for line in gt_test:
-        filename = (line[0])
-        count = int(line[1])
-        gt_pts_test.append([filename, count])
+gt_pts_test = []
+for line in gt_test:
+    filename = (line[0])
+    count = int(line[1])
+    gt_pts_test.append([filename, count])
 
-    gt_test_sorted = sorted(gt_pts_test, key=lambda tup: tup[0])
-    test_x = column(gt_test_sorted, 1)
+gt_test_sorted = sorted(gt_pts_test, key=lambda tup: tup[0])
+test_x = column(gt_test_sorted, 1)
 
-    # eps_arr = [1, 2.5, 5, 7.5, 10]
-    # min_pts_arr = [1, 2, 3, 4, 5]
-    eps_arr = np.linspace(1,10,46) #[1, 2,3,4,5,6,7,8,9, 10]
-    # eps_arr = [x * 0.2 for x in range(1, 10)]
-    min_pts_arr = [1, 2, 3, 4, 5]
+# eps_arr = [1, 2.5, 5, 7.5, 10]
+# min_pts_arr = [1, 2, 3, 4, 5]
+# eps_arr = np.linspace(5,10,30) #[1, 2,3,4,5,6,7,8,9, 10]
+eps_arr = np.linspace(1,10,45) #[1, 2,3,4,5,6,7,8,9, 10]
 
-    output = []
-    for eps in eps_arr:
-        eps = round(eps,1)
-        for min_pts in min_pts_arr:
-            cluster_pts = cluster_all(eps, min_pts,len(gt_test)) 
+# eps_arr = [x * 0.2 for x in range(1, 10)]
+min_pts_arr = range(1,len(p2ptr_names))#[1, 2, 3, 4, 5]
 
-            cluster_pts_sorted = sorted(cluster_pts, key=lambda tup: tup[0])
-            test_y = column(cluster_pts_sorted, 1) 
+output = []
+# smallest = 1
+for eps in eps_arr:
+    eps = round(eps,1)
+    for min_pts in min_pts_arr:
+        cluster_pts = cluster_all(eps, min_pts,len(gt_test)) 
 
-            correct = 0
-            for i in range(len(test_y)):
-                if test_y[i] == test_x[i]:
-                    correct += 1
+        cluster_pts_sorted = sorted(cluster_pts, key=lambda tup: tup[0])
+        test_y = column(cluster_pts_sorted, 1) 
 
-            acc = round(correct/len(gt_test),5)
+        correct = 0
+        for i in range(len(test_y)):
+            if test_y[i] == test_x[i]:
+                correct += 1
 
-            mae = round(metrics.mean_absolute_error(test_x,test_y),5)
-            mse = round(metrics.mean_squared_error(test_x,test_y),5)
+        acc = round(correct/len(gt_test),5)
+
+        mae = round(metrics.mean_absolute_error(test_x,test_y),5)
+        mse = round(metrics.mean_squared_error(test_x,test_y),5)
+
+        # if mae < smallest:
+        #     smallest = mae
+        #     output.append([eps, min_pts, mse, mae, acc])
+
+        if mae < .4:
             output.append([eps, min_pts, mse, mae, acc])
 
-    with open('clus_scan_out.txt', 'w') as f:
-        f.write("eps min_pts  mse  mae  accuracy\n")
-        for line in output:
-            f.write(f"{line}\n")
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser('Deer database split script', parents=[get_args_parser()])
-    args = parser.parse_args()
-    main(args)
+with open('clus_scan_out.txt', 'w') as f:
+    f.write("eps min_pts  mse  mae  accuracy\n")
+    for line in output:
+        f.write(f"{line}\n")
